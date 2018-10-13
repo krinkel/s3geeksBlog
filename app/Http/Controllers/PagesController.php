b1670e344bbf81762c2c0148367c4327ac0aff51
Ahmed AbdElrazek Ahmed <admin@yallagroup.net>
2018-10-13 18:47:06 +0200

    add empty pages methods and views

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('pages.home');
+    }
+
+    public function about()
+    {
+        return view('pages.about');
+    }
+
+    public function contact()
+    {
+        return view('pages.contact');
+    }
+
+    public function sendMessage()
+    {
+        
+    }
 }
